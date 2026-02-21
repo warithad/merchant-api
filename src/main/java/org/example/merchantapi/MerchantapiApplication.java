3de584f5380310247bf13bd9d86de92cb18db95b
mula⭐️ <bstar@Abduls-MacBook-Air.local>
2026-02-21 11:10:01 +0100

    Initial commit

---
+++ src/main/java/org/example/merchantapi/MerchantapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.merchantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/merchantapi/MerchantapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.merchantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
